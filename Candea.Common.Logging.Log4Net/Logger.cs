0274d84879cf23b5c748e74d9996e63cb12a04df
Ian Schlarman <ischlarman@gmail.com>
2019-04-26 13:32:49 -0500

    Initial version. Port to .NET Core and Standard pending.

---
+++ Candea.Common.Logging.Log4Net/Logger.cs
@@ -0,0 +1,128 @@
+﻿/*
+This source file is under MIT License (MIT)
+Copyright (c) 2017 Mihaela Iridon
+https://opensource.org/licenses/MIT
+*/
+
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace Candea.Common.Logging
+{
+
+    /// <summary>
+    /// The logging implementation class which only redirects the log calls to the log4net logger.
+    /// This was added in order to separate all projects (Except this one) from the logging technology used (log4net).
+    /// Note: all logging takes place on separate threads, to improve performance (configuration-based option)
+    /// </summary>
+    public class Logger : ILogger
+    {
+        /// <summary>
+        /// The backing log4net logger implementation.
+        /// </summary>
+        protected ILog Logr = null;
+
+        /// <summary>
+        /// Instantiates a new <see cref="Logger"/> with the provided <paramref name="name"/>.
+        /// </summary>
+        /// <param name="name"></param>
+        public Logger(string name)
+        {
+            Logr = LogManager.GetLogger(name);
+        }
+
+        /// <summary>
+        /// Instantiates a new <see cref="Logger"/> identified by the provided <see cref="Type"/>.
+        /// </summary>
+        /// <param name="t"></param>
+        public Logger(Type t)
+        {
+            Logr = LogManager.GetLogger(t);
+        }
+
+        /// <summary>
+        /// Logs a Trace <paramref name="message"/> with an optional <paramref name="exception"/>.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        /// <param name="exception">An optional <see cref="Exception"/> to log details from.</param>
+        public void Trace(object message, Exception exception = null) =>
+            Execute(() => Logr.Debug(message, exception));
+
+        /// <summary>
+        /// Logs a Debug <paramref name="message"/> with an optional <paramref name="exception"/>.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        /// <param name="exception">An optional <see cref="Exception"/> to log details from.</param>
+        public void Debug(object message, Exception exception = null) =>
+            Execute(() => Logr.Debug(message, exception));
+
+        /// <summary>
+        /// Logs an Info <paramref name="message"/> with an optional <paramref name="exception"/>.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        /// <param name="exception">An optional <see cref="Exception"/> to log details from.</param>
+        public void Info(object message, Exception exception = null) =>
+            Execute(() => Logr.Info(message, exception));
+
+        /// <summary>
+        /// Logs a Warn <paramref name="message"/> with an optional <paramref name="exception"/>.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        /// <param name="exception">An optional <see cref="Exception"/> to log details from.</param>
+        public void Warn(object message, Exception exception = null) =>
+            Execute(() => Logr.Warn(message, exception));
+
+        /// <summary>
+        /// Logs an Error <paramref name="message"/> with an optional <paramref name="exception"/>.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        /// <param name="exception">An optional <see cref="Exception"/> to log details from.</param>
+        public void Error(object message, Exception exception = null) =>
+            Execute(() => Logr.Error(message, exception));
+
+        /// <summary>
+        /// Logs a Fatal <paramref name="message"/> with an optional <paramref name="exception"/>.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        /// <param name="exception">An optional <see cref="Exception"/> to log details from.</param>
+        public void Fatal(object message, Exception exception = null) =>
+            Execute(() => Logr.Fatal(message, exception));
+
+        /// <summary>
+        /// Logs a general <paramref name="message"/> with an optional <paramref name="exception"/> using the provided level.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        /// <param name="exception">An optional <see cref="Exception"/> to log details from.</param>
+        /// <param name="level">The logging level. <see cref="LogLevel.Trace"/> by default.</param>
+        public void Log(object message, Exception exception = null, LogLevel level = LogLevel.Trace) =>
+            Execute(() => _delegates[level](Logr, message, exception));
+
+
+        private static void Execute(Action a)
+        {
+            if (LoggingManager.RunOnSeparateThread)
+            { 
+                Task.Factory.StartNew(a);
+            }
+            else
+            {
+                a();
+            }
+        }
+
+        private static readonly IDictionary<LogLevel, Action<ILog, object, Exception>> _delegates = 
+            new ReadOnlyDictionary<LogLevel, Action<ILog, object, Exception>>(
+                new Dictionary<LogLevel, Action<ILog, object, Exception>>
+                {
+                    [LogLevel.Trace] = (l,m,e) => l.Debug(m,e),
+                    [LogLevel.Debug] = (l, m, e) => l.Debug(m, e),
+                    [LogLevel.Info] = (l, m, e) => l.Info(m, e),
+                    [LogLevel.Warn] = (l, m, e) => l.Warn(m, e),
+                    [LogLevel.Error] = (l, m, e) => l.Error(m, e),
+                    [LogLevel.Fatal] = (l, m, e) => l.Fatal(m, e)
+                });
+    }
+}
---
+++ Candea.Common.Logging.Log4Net/LoggingManager.cs
@@ -0,0 +1,166 @@
+﻿/*
+This source file is under MIT License (MIT)
+Copyright (c) 2017 Mihaela Iridon
+https://opensource.org/licenses/MIT
+*/
+
+using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Reflection;
+using static Candea.Common.Util.auxfunc;
+using static log4net.Config.XmlConfigurator;
+using static System.Diagnostics.Debug;
+
+namespace Candea.Common.Logging
+{
+    /// <summary>
+    /// An implementation of the ILogManager to deal with the log4net logging framework; specifically the initialization and shutting down of the logger
+    /// </summary>
+    [Export(typeof(ILogManager))]
+    public class LoggingManager : ILogManager, IDisposable
+    {
+        internal static readonly bool RunOnSeparateThread = false;
+        private const string LogConfig = "log4net.config";
+
+        static LoggingManager()
+        {
+            var crtConfigFile = AppDomain.CurrentDomain.SetupInformation.ConfigurationFile;
+            var crtConfigFilePath = Path.GetDirectoryName(crtConfigFile);
+            var crtConfigFileName = Path.GetFileName(crtConfigFile);
+            var l4nc = crtConfigFileName.Replace("dll", "log4net");
+            var asm = Assembly.GetEntryAssembly();
+            var asmName = asm?.GetName().Name ?? l4nc;
+
+            var headerInsert = asmName.Equals("web.config", StringComparison.InvariantCultureIgnoreCase)
+                ? "Web Application" : asmName;
+
+            var header = appSetting("Logging.Header", $"_____________ {headerInsert} _____________");
+            var configFileName = CheckLog4NetFileOrCreate(asmName, crtConfigFilePath);
+
+            RunOnSeparateThread = appSetting("Logging.RunOnSeparateThread", true);
+            try
+            {
+                if (!string.IsNullOrEmpty(configFileName))
+                {
+                    var configFile = new FileInfo(Path.Combine(Environment.CurrentDirectory, configFileName));
+                    if (configFile.Exists)
+                        Configure(configFile);
+                }
+                else
+                    Configure();
+            }
+            catch(Exception e)
+            {
+                WriteLine($"ERROR: Could not initialize logging: {e.Message}", e);
+                return;
+            }
+
+            _localLogger = log4net.LogManager.GetLogger(typeof(LoggingManager));
+            _localLogger.Info(header);
+            _localLogger.Debug($"Logging messages on separate thread={RunOnSeparateThread}");
+        }
+
+        /// <summary>
+        /// Produces a new <see cref="ILogger"/> implementation with the given <paramref name="name"/>.
+        /// </summary>
+        /// <param name="name">Name of the logger to create.</param>
+        /// <returns>An <see cref="ILogger"/> implementation with the given <paramref name="name"/>.</returns>
+        public static ILogger Logger(string name) => new Logger(name) as ILogger;
+
+        /// <summary>
+        /// Produces a new <see cref="ILogger"/> implementation with the given <paramref name="name"/>.
+        /// </summary>
+        /// <param name="name">Name of the logger to create.</param>
+        /// <returns>An <see cref="ILogger"/> implementation with the given <paramref name="name"/>.</returns>
+        public ILogger GetLogger(string name) => Logger(name);
+        
+        /// <summary>
+        /// Produces a new <see cref="ILogger"/> implementation identified by the <see cref="Type"/> of <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">Used to identify the logging source.</typeparam>
+        /// <returns>An <see cref="ILogger"/> implementation.</returns>
+        public static ILogger Logger<T>() where T : class => new Logger(typeof(T)) as ILogger;
+
+        /// <summary>
+        /// Produces a new <see cref="ILogger"/> implementation identified by the <see cref="Type"/> of <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">Used to identify the logging source.</typeparam>
+        /// <returns>An <see cref="ILogger"/> implementation.</returns>
+        public ILogger GetLogger<T>() where T : class => Logger<T>();
+
+        /// <summary>
+        /// Produces a new <see cref="ILogger"/> implementation identified by the provided <paramref name="type"/>.
+        /// </summary>
+        /// <returns>An <see cref="ILogger"/> implementation.</returns>
+        public static ILogger Logger(Type type) => new Logger(type) as ILogger;
+
+        /// <summary>
+        /// Produces a new <see cref="ILogger"/> implementation identified by the provided <paramref name="type"/>.
+        /// </summary>
+        /// <returns>An <see cref="ILogger"/> implementation.</returns>
+        public ILogger GetLogger(Type type) => Logger(type);
+
+        /// <summary>
+        /// Closes the backing logging implementation.
+        /// </summary>
+        public static void ShutdownLogger() => LogAndShutdownLogger();
+
+        /// <summary>
+        /// Closes the backing logging implementation.
+        /// </summary>
+        public void Shutdown() => ShutdownLogger();
+
+        /// <summary>
+        /// Closes the backing logging implementation.
+        /// </summary>
+        public void Dispose() => ShutdownLogger();
+
+
+        private static readonly log4net.ILog _localLogger;
+
+        private static void LogAndShutdownLogger()
+        {
+            _localLogger.Info(">>>>> Shutting down log4net Logger. BYE.");
+            log4net.LogManager.Shutdown();
+        }
+
+        private static string ReadLog4NetConfigEmbeddedResourceFile()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName =  $"Shared.Frameworks.Logging.Log4Net.{LogConfig}";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string result = reader.ReadToEnd();
+                return result;
+            }
+        }
+
+        private static string CheckLog4NetFileOrCreate(string execAsm, string path)
+        {
+            bool isWebConfig = execAsm.Equals("web.config", StringComparison.InvariantCultureIgnoreCase);
+
+            var configFileName = appSetting<string>("Logging.ConfigFilePath");
+            if (configFileName == null || !File.Exists(configFileName))
+            {
+                configFileName = Path.Combine(path, isWebConfig ? LogConfig : execAsm);
+
+                if (!File.Exists(configFileName))
+                {
+                    //replace "DefaultLoggingService" in the local file with execAsm.log4net.config and create a copy naming it like that
+                    var s = ReadLog4NetConfigEmbeddedResourceFile();
+                    if (s == null)
+                    {
+                        return null;
+                    }
+                    var r = s.Replace("DefaultLoggingService.", isWebConfig ? "WebAppLogger." : execAsm.Replace(LogConfig, string.Empty));
+                    File.WriteAllText(configFileName, r);
+                }
+            }
+            return configFileName;
+        }
+    }
+
+}
